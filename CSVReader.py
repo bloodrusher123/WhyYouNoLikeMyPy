714512c33f553bdd1a3fc68effc616fdc6eddbc1
bloodrusher123 <bloodrusher123@users.noreply.github.com>
2019-10-28 08:37:34 +0000

    Update CSVReader.py

--- CSVReader.py
+++ CSVReader.py
@@ -9,14 +9,19 @@ outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 inbox = outlook.GetDefaultFolder(6)
 messages =inbox.items
 
-for message in messages:
 file = "C:/Users/jbateman/Documents/TestCSV.csv"
 
 with open(file) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     line_count =0
     for row in csv_reader:
-        if (row[0]) == 'BECY34PC01':
-            print(row)
-        else:
-            print("")
+        for message in messages:
+            if row[1] in message.body:
+                NewMsg = message.Forward()
+                NewMsg.body = message.body
+                NewMsg.subject = message.subject
+                NewMsg.To = DocInsureMailbox
+                NewMsg.send()
+                print("Yeah")
+            else:
+                print("")
