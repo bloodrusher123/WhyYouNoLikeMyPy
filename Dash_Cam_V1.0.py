7023da735a7926186cbbed020c91b3827f625808
bloodrusher123 <bloodrusher123@users.noreply.github.com>
2022-10-12 10:52:35 +0100

    Update Dash_Cam_V1.0.py

--- Dash_Cam_V1.0.py
+++ Dash_Cam_V1.0.py
@@ -5,7 +5,7 @@ import datetime
 camera = PiCamera()
 
 camera.resolution = (1280, 720)
-camera.framerate = 15
+camera.framerate = 30
 camera.start_preview()
 sleep(5)
 camera.start_recording('/home/pi/desktop/video.h264'+datetime.now())
