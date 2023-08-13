c9e80ea2c106a7825c282cf8752403344d8abb76
bloodrusher123 <bloodrusher123@users.noreply.github.com>
2023-08-13 17:39:28 +0100

    Add files via upload

---
+++ logicClaimNumbers.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+
+videoFileExtensions = ['mp4', 'wmv', 'mov', 'avi', 'mkv', 'flv', 'avchid', 'f4v', 'swf']
+
+def moveVideoFiles(listFilePaths, destinationPath):
+    for i in listFilePaths:
+        for j in videoFileExtensions:
+            if j in i:
+                shutil.move(i, destinationPath)
+                print('Moved ' + i + ' to ' + j)
+
+def filterClaimNumbers(blobList):
+    claimNumberList = list(set(blobList))
+    print(claimNumberList)
+    return claimNumberList
+
+def createFolder(claimNumber):
+    path = 'C://downloads/' + claimNumber
+    if not os.path.exists(path):
+        os.mkdir(path)
+        print('Folder %s created' % path)
+    else:
+        print('Folder %s already exists' % path)
+    
